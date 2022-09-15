79ffdcd0432956678037e5a495c17591706162c8
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-15 17:17:22 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+
+function App () {
+  return (
+    <Box minH='100vh' bg='gray.700'>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita iusto, pariatur nostrum iste, delectus odio, eveniet hic debitis tenetur ipsum labore. Voluptates rerum quas iusto ipsum id vel sit ratione?</p>
+    </Box>
+  )
+}
+
+export default App
