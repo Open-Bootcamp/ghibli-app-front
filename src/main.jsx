98cbb3d724deea6c121b42d9048c947a3165520e
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-09-16 16:15:25 -0400

    add fonts and fetch ghibli-api

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import theme from './theme/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
--- src/theme/index.js
+++ src/theme/index.js
@@ -2,11 +2,11 @@ import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
-    body: 'Inter,sans-serif'
+    body: 'Inter, sans-serif'
   },
   fontWeights: {
     normal: 400,
-    medium: 600,
+    medium: 500,
     bold: 700
   }
 })
