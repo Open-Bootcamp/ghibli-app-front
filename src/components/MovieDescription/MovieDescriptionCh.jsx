969aa7ed2833b0483e8cce248740a02b743d3342
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-09-18 15:08:05 -0400

    ADD: Characters
    Adding characters components

---
+++ src/components/MovieDescription/MovieDescriptionCh.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CharactersContainer from './CharacterContainer'
+import MovieMenu from './MovieMenu'
+
+const MovieDescriptionCh = () => {
+  return (
+    <>
+      <MovieMenu />
+      <CharactersContainer />
+    </>
+  )
+}
+
+export default MovieDescriptionCh
