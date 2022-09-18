ec1054c29ecb9ce7f624db0fff6bb35c2eea36e9
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-09-18 14:38:42 -0400

    ADD: Movie Description Adding movie description

---
+++ src/components/Movie Description/MovieDescription.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Information from './Information'
+import MovieMenu from './MovieMenu'
+
+function MovieDescription () {
+  return (
+    <>
+      <MovieMenu />
+      <Information />
+    </>
+
+  )
+}
+
+export default MovieDescription
