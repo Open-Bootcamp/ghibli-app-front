402b5055aba9bd57ec309e7044a88ecde68d79df
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-09-19 18:36:03 -0400

    ADD: Loader component
    Adding loader component in the app

---
+++ src/components/Common/Loader.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@chakra-ui/react'
+import './loader.css'
+
+function Loader () {
+  return (
+    <Stack py={12}>
+      <div class='loader' />
+    </Stack>
+  )
+}
+
+export default Loader
--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -2,6 +2,7 @@ import { Box, Text, Grid, Container, Link } from '@chakra-ui/react'
 import { Link as Linked } from 'react-router-dom'
 import CardMovie from './CardMovie'
 import GetMovies from '../services/useGetMovies'
+import Loader from './Common/Loader'
 
 function MovieContainer () {
   const data = GetMovies()
@@ -11,13 +12,15 @@ function MovieContainer () {
       <Container margin='0 auto' maxW='container.xl'>
         <Text color='white' py='30px' pl='7' fontSize='30px' fontWeight='800'>Popular Movies</Text>
         <Grid gridTemplateColumns='repeat(auto-fit, minmax(290px, 1fr))' gap='2' placeItems='center'>
-          {data.map((movie) => {
-            return (
-              <div key={movie._id}>
-                <Link as={Linked} to={`/movie/${movie._id}`}><CardMovie movie={movie} /></Link>
-              </div>
-            )
-          })}
+          {data.length === 0
+            ? <Loader />
+            : (data.map((movie) => {
+                return (
+                  <div key={movie._id}>
+                    <Link as={Linked} to={`/movie/${movie._id}`}><CardMovie movie={movie} /></Link>
+                  </div>
+                )
+              }))}
         </Grid>
       </Container>
     </Box>
