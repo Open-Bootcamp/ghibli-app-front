9999aa4fded68700663548cd7065aad9c164c853
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-09-18 20:03:18 -0400

    ➕ADD: Banner Component
    Adding Banner to better understanding of component at Home

---
+++ src/components/Banner.jsx
@@ -0,0 +1,65 @@
+import {
+  Box, Flex, Stack, Text
+} from '@chakra-ui/react'
+import { AiFillStar } from 'react-icons/ai'
+import Carousel from './Carousel'
+
+function Banner () {
+  const imgBanner =
+    'https://image.tmdb.org/t/p/original/etqr6fOOCXQOgwrQXaKwenTSuzx.jpg'
+
+  return (
+    <Flex
+      w='full'
+      h='100vh'
+      backgroundImage={imgBanner}
+      backgroundSize='cover'
+      backgroundPosition='center center'
+    >
+      <Flex
+        w='full'
+        justify='space-between'
+        align='center'
+        px={{ base: 6, md: 20 }}
+        bgGradient='linear(to-r, blackAlpha.900, transparent)'
+      >
+        <Stack maxW='2xl' align='flex-start' spacing={12}>
+          <Text
+            color='white'
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={{ base: '4xl', md: '6.5rem' }}
+            pb={4}
+          >
+            My Neighbor Totoro
+          </Text>
+          <Stack direction='row' align='center' gap={6}>
+            <Box display='flex' alignItems='center' gap={2}>
+              <AiFillStar color='yellow' />
+              <Text fontSize='3vh' as='b' color='white'>
+                9.3
+              </Text>
+            </Box>
+            <Box>
+              <Text fontSize='3vh' as='b' color='white'>
+                1h 43m | Producer - Hayao Miyazaki
+              </Text>
+            </Box>
+          </Stack>
+          <Box>
+            <Text color='white' fontSize='2vh' as='b'>
+              Two sisters move to the country with their father in order to be
+              closer to their hospitalized mother, and discover the surrounding
+              trees are by Totoros, magical spirits of the forest. When the
+              youngest runs away from home, the older sister seeks help from
+              home, the older sister seeks help from the spirits to find her.
+            </Text>
+          </Box>
+        </Stack>
+        <Carousel />
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Banner
